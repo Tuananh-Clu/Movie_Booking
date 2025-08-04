c3396623f7052804ff49ba034d21738c264dfaaf
Tuananh-Clu <yianh798@gmail.com>
2025-08-04 15:12:30 +0700

    fix sd

--- src/components/SeatsComponents/SeatM.tsx
+++ src/components/SeatsComponents/SeatM.tsx
@@ -50,17 +50,17 @@ export const SeatM = React.memo(
           const isOrdered = item.isSelected;
           const isVip = vipRow.includes(item.id.charAt(0));
           let baseColor = "bg-green-500";
-          if (isOrdered) baseColor = "bg-red-600";
-          if (item.isSelected == true) baseColor = "bg-red-600";
-          else if (isSelected) baseColor = "bg-yellow-400";
-          else if (
-            seat.some(
-              (i) =>
-                i.id === item.id &&
-                i.roomId === currentRoom?.name &&
-                i.movieTitle === decodedTitle
-            )
-          ) {
+           if (isOrdered) baseColor = "bg-red-600";
+                    if(item.isOrdered==true) baseColor = "bg-red-600";
+                    else if (isSelected) baseColor = "bg-yellow-400";
+                    else if (
+                      seat.some(
+                        (i) =>
+                          i.id === item.id &&
+                          i.roomId === currentRoom?.name &&
+                          i.movieTitle === decodedTitle
+                      )
+                    ) {
             baseColor = "bg-yellow-400";
           }
 
@@ -70,7 +70,7 @@ export const SeatM = React.memo(
                           onClick={() =>
                             handleClick(
                               item.id,
-                              item.isSelected==true,
+                              item.isOrdered==true,
                               seatDates.slice(1, seatDates?.length).toString(),
                               currentRoom?.name || ids || "",
                               isVip ? 100000 : 75000,
