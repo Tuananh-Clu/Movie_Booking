61c215ad3c3f127387f99ad23fc87e6cf81be609
Tuananh-Clu <yianh798@gmail.com>
2025-08-06 10:07:41 +0700

    da

--- src/components/Profile/Admin/AddPhim.tsx
+++ src/components/Profile/Admin/AddPhim.tsx
@@ -27,7 +27,7 @@ export const AddPhim = () => {
 
       {/* Scrollable list */}
       <div className="relative">
-        <div className="scrollbar-hide flex gap-4 overflow-x-auto pb-2 snap-x snap-mandatory">
+        <div className="scrollbar-hide flex gap-4 hide-scrollbar max-w-[1200px]d overflow-x-auto pb-2 snap-x snap-mandatory">
           {moviesPlaying
             .filter((item) => item.backdrop_path)
             .map((item, index) => (
